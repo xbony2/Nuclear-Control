7f9a8662bfcd8b34691fd0338b1dc899e49d2c53
xbony2 <xbony2@users.noreply.github.com>
2015-04-26 08:54:58 -0400

    Update CrossIndustrialCraft2Classic.java

--- src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/ic2classic/CrossIndustrialCraft2Classic.java
+++ src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/ic2classic/CrossIndustrialCraft2Classic.java
@@ -3,19 +3,26 @@ package shedar.mods.ic2.nuclearcontrol.crossmod.ic2classic;
 import shedar.mods.ic2.nuclearcontrol.utils.NCLog;
 
 public class CrossIndustrialCraft2Classic{
-	private boolean isIC2ClassicOn;
+	private IC2ClassicType type;
 	
 	public CrossIndustrialCraft2Classic(){
 		try{
 			Class classicClass = Class.forName("ic2.api.info.IC2Classic");
-			isIC2ClassicOn = true;
+			type = IC2ClassicType.SPEIGER;
 			NCLog.warn("IC2 Classic detected: issues may occur. Especially with Speiger's terrible indent style :P");
 		}catch(ClassNotFoundException e){
-			isIC2ClassicOn = false;
+			try{
+				Class classicClass = Class.forName("ic2classic.core.IC2");
+				type = IC2ClassicType.IMMIBIS;
+				NCLog.warn("IC2 Classic detected: issues may occur. Especially since Immibis "
+						+ "doesn't know how to created new packages :P");
+			}catch(ClassNotFoundException e1){
+				type = IC2ClassicType.NONE;
+			}
 		}
 	}
 	
-	public boolean isIc2ClassicOn(){
-		return isIC2ClassicOn;
+	public IC2ClassicType getClassicType(){
+		return type;
 	}
 }
