7c7ad7b103a6cb6b9ad6846b6f22e1aa9fa489d6
maggi373 <40539743+maggi373@users.noreply.github.com>
2022-05-23 00:39:21 +0200

    remove the comment out i made

--- src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/appeng/TileEntityNetworkLink.java
+++ src/main/java/shedar/mods/ic2/nuclearcontrol/crossmod/appeng/TileEntityNetworkLink.java
@@ -94,7 +94,6 @@ public class TileEntityNetworkLink extends AENetworkTile {
             } else if(tile instanceof TileChest){
                 TileChest chest = (TileChest) tile;
                 ItemStack is = chest.getInternalInventory().getStackInSlot(0);
-                // ItemStack is = null; // FIXME
                 if(is != null){
                     IMEInventoryHandler inventory = AEApi.instance().registries().cell().getCellInventory( is, null, StorageChannel.ITEMS );
                     if(inventory instanceof ICellInventoryHandler){
